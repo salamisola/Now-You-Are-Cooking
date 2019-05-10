3f0e14456cfad9833f6c5aa696c2d73140fd1e46
jcnimble <jcginelli@outlook.com>
2019-05-10 13:20:38 -0400

    config

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/recipe-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
