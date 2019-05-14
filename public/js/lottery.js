cfad1e73f043f76b2539c7b056caa32997ee633b
Amir P <apourrezaei@gmail.com>
2019-05-14 19:00:39 -0400

    Lottery Single

--- public/js/lottery.js
+++ public/js/lottery.js
@@ -9,5 +9,22 @@ $.ajax(settings).done(function (response) {
     var rndRecId = Math.floor(Math.random() * rndMax+1);
     console.log("Random Max: " + rndMax);
     console.log("Random Recipe ID: " + rndRecId);
+    let lotteryPick;
+    $.ajax("/api/recipes/" + rndRecId, {
+        type: "GET",
+        async: true
+    }).then(
+        function (response) {
+            $("#divLottery").empty();
+            lotteryPick = JSON.stringify(response);
+            // console.log(lotteryPick);
+            var p = $("<p></p>");
+            console.log(response.title);
+            p.text(response.title);
+            $("#divLottery").append(p);
+            
+        }
+    );
+    
 
 });
